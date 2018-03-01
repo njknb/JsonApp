6eba511fdc3126fa424ec0a4d06f28d12474ed43
njknb <1134718450@qq.com>
2018-03-01 17:03:08 +0800

    Initial commit

---
+++ app/src/main/java/jsonapp/com/example/hy/jsonapp/MainActivity.java
@@ -0,0 +1,18 @@
+package jsonapp.com.example.hy.jsonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.alibaba.fastjson.JSON;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
